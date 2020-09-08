8aef0fac827fb98b4417824fd99560756e41c5a5
Hongwen <hongwen.zhang@cripac.ia.ac.cn>
2020-09-08 23:15:26 +0800

    support evaluation

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
+"""
+This script can be used to produce demo results.
+Example usage:
+```
+python demo.py  --cfg configs/danet_h36m_itw.yaml --load_ckpt ./data/pretrained_model/danet_model_h36m_itw.pth --img_dir ./examples --use_opendr
+```
+"""
 from __future__ import absolute_import
 from __future__ import division
 
@@ -17,17 +24,18 @@ import numpy as np
 import torch
 import torchvision
 from torchvision.utils import make_grid
+import torch.nn as nn
 
-import lib.nn as mynn
-import lib.utils.net as net_utils
-from lib.core.config import cfg, cfg_from_file
-from lib.modeling.danet import DaNet
-from lib.utils.logging import setup_logging
+from models import SMPL
+from models.core.config import cfg, cfg_from_file
+from models.danet import DaNet
 from skimage.transform import resize
-from lib.utils.iuvmap import iuv_map2img
+from utils.iuvmap import iuv_map2img
 
-# Set up logging and load config options
-logger = setup_logging(__name__)
+import path_config
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 def parse_args():
@@ -35,18 +43,14 @@ def parse_args():
     parser = argparse.ArgumentParser(description='DaNet for 3D Human Shape and Pose')
 
     parser.add_argument(
-        '--cfg', dest='cfg_file', default='configs/smpl/smpl_encoder.yaml',
+        '--cfg', dest='cfg_file', default='configs/danet_h36m_itw.yaml',
         help='config file for training / testing')
-
     parser.add_argument(
-        '--load_ckpt', help='checkpoint path to load')
-
+        '--checkpoint', help='checkpoint path to load')
     parser.add_argument(
-        '--img_dir', default='./imgs', type=str, help='path to test images')
-
+        '--img_dir', default='./examples', type=str, help='path to test images')
     parser.add_argument(
         '--out_dir', default='./output', type=str, help='path to output results')
-
     parser.add_argument(
         '--use_opendr', help='use opendr renderer to visualize results', action='store_true')
 
@@ -61,32 +65,37 @@ def main():
 
     cfg.DANET.REFINEMENT = EasyDict(cfg.DANET.REFINEMENT)
     cfg.MSRES_MODEL.EXTRA = EasyDict(cfg.MSRES_MODEL.EXTRA)
+    cfg.batch_size = 1
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    if cfg.DANET.SMPL_MODEL_TYPE == 'male':
+        smpl_male = SMPL(path_config.SMPL_MODEL_DIR,
+                         gender='male',
+                         create_transl=False).to(device)
+        smpl = smpl_male
+    elif cfg.DANET.SMPL_MODEL_TYPE == 'neutral':
+        smpl_neutral = SMPL(path_config.SMPL_MODEL_DIR,
+                            create_transl=False).to(device)
+        smpl = smpl_neutral
+    elif cfg.DANET.SMPL_MODEL_TYPE == 'female':
+        smpl_female = SMPL(path_config.SMPL_MODEL_DIR,
+                           gender='female',
+                           create_transl=False).to(device)
+        smpl = smpl_female
 
     if args.use_opendr:
-        from lib.utils.opendr_render import opendr_render
-        if cfg.DANET.SMPL_MODEL_TYPE == 'male':
-            smpl_model_path = './data/SMPL_data/basicmodel_m_lbs_10_207_0_v1.0.0.pkl'
-        elif cfg.DANET.SMPL_MODEL_TYPE == 'neutral':
-            smpl_model_path = './data/SMPL_data/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl'
-        elif cfg.DANET.SMPL_MODEL_TYPE == 'female':
-            smpl_model_path = './data/SMPL_data/basicModel_f_lbs_10_207_0_v1.0.0.pkl'
-        dr_render = opendr_render(model_path=smpl_model_path)
+        from utils.opendr_render import opendr_render
+        dr_render = opendr_render(ratio=1)
 
     if not os.path.exists(args.out_dir):
         os.makedirs(args.out_dir)
 
     ### Model ###
-    model = DaNet().cuda()
+    model = DaNet(cfg, path_config.SMPL_MEAN_PARAMS).to(device)
 
-    ### Load checkpoint
-    if args.load_ckpt:
-        load_name = args.load_ckpt
-        logging.info("loading checkpoint %s", load_name)
-        checkpoint = torch.load(load_name, map_location=lambda storage, loc: storage)
-        net_utils.load_ckpt(model, checkpoint)
-        del checkpoint
-
-    model = mynn.DataParallel(model, minibatch=False)
+    checkpoint = torch.load(args.checkpoint)
+    model.load_state_dict(checkpoint['model'], strict=False)
     model.eval()
 
     img_path_list = [os.path.join(args.img_dir, name) for name in os.listdir(args.img_dir) if name.endswith('.jpg')]
@@ -98,17 +107,11 @@ def main():
         image_tensor = torchvision.transforms.ToTensor()(image).unsqueeze(0).cuda()
 
         # run inference
-        pred_results = model.module.infer_net(image_tensor)
-
-        para_pred = pred_results['para']
-
-        cam_pred = para_pred[:, 0:3].contiguous()
-        beta_pred = para_pred[:, 3:13].contiguous()
-        Rs_pred = para_pred[:, 13:].contiguous().view(-1, 24, 3, 3)
-
-        smpl_pts = model.module.iuv2smpl.smpl(beta_pred, Rs=Rs_pred, get_skin=True)
-        kps3ds_pred = smpl_pts['cocoplus']
-        vert_pred = smpl_pts['verts']
+        pred_dict = model.infer_net(image_tensor)
+        para_pred = pred_dict['para']
+        camera_pred = para_pred[:, 0:3].contiguous()
+        betas_pred = para_pred[:, 3:13].contiguous()
+        rotmat_pred = para_pred[:, 13:].contiguous().view(-1, 24, 3, 3)
 
         # input image
         image_np = image_tensor[0].cpu().numpy()
@@ -120,13 +123,13 @@ def main():
         image_in_rgba = np.concatenate((image_np, ones_np), axis=2)
 
         # estimated global IUV
-        global_iuv = iuv_map2img(*pred_results['visualization']['iuv_pred'])[0].cpu().numpy()
+        global_iuv = iuv_map2img(*pred_dict['visualization']['iuv_pred'])[0].cpu().numpy()
         global_iuv = np.transpose(global_iuv, (1, 2, 0))
         global_iuv = resize(global_iuv, image_np.shape[:2])
         global_iuv_rgba = np.concatenate((global_iuv, ones_np), axis=2)
 
         # estimated patial IUV
-        part_iuv_pred = pred_results['visualization']['part_iuv_pred'][0]
+        part_iuv_pred = pred_dict['visualization']['part_iuv_pred'][0]
         p_iuv_vis = []
         for i in range(part_iuv_pred.size(0)):
             p_u_vis, p_v_vis, p_i_vis = [part_iuv_pred[i, iuv].unsqueeze(0) for iuv in range(3)]
@@ -134,7 +137,7 @@ def main():
                 p_iuv_vis_i = iuv_map2img(p_u_vis.detach(), p_v_vis.detach(), p_i_vis.detach())
             else:
                 p_iuv_vis_i = iuv_map2img(p_u_vis.detach(), p_v_vis.detach(), p_i_vis.detach(),
-                                          ind_mapping=[0] + model.module.img2iuv.dp2smpl_mapping[i])
+                                          ind_mapping=[0] + model.img2iuv.dp2smpl_mapping[i])
             p_iuv_vis.append(p_iuv_vis_i)
         part_iuv = torch.cat(p_iuv_vis, dim=0)
         part_iuv = make_grid(part_iuv, nrow=6, padding=0).cpu().numpy()
@@ -143,9 +146,12 @@ def main():
                                            axis=2)
 
         # rendered IUV of the predicted SMPL model
-        smpl_projection = model.module.iuv2smpl.make_uv_image(Rs=Rs_pred[0].unsqueeze(0), beta=beta_pred[0].unsqueeze(0),
-                                                             cam=cam_pred[0].unsqueeze(0), add_smpl_joint=True)
-        render_iuv = smpl_projection['render_image'].squeeze(0).cpu().numpy()
+        smpl_output = smpl(betas=betas_pred, body_pose=rotmat_pred[:, 1:],
+                                   global_orient=rotmat_pred[:, 0].unsqueeze(1), pose2rot=False)
+        verts_pred = smpl_output.vertices
+        render_iuv = model.iuv2smpl.verts2uvimg(verts_pred[0].unsqueeze(0), cam=camera_pred[0].unsqueeze(0))
+        render_iuv = render_iuv[0].cpu().numpy()
+
         render_iuv = np.transpose(render_iuv, (1, 2, 0))
         render_iuv = resize(render_iuv, image_np.shape[:2])
 
@@ -158,23 +164,19 @@ def main():
 
         if args.use_opendr:
             # visualize the predicted SMPL model using the opendr renderer
-            joint_pelvis = torch.mean(kps3ds_pred[:, [2, 3]], dim=1).unsqueeze(1)
-            vert_centered = vert_pred - joint_pelvis
-
-            K, _, _ = model.module.iuv2smpl.camera_matrix(cam_pred)
-            _, _, img_smpl, smpl_rgba = dr_render.render(
-                image_tensor[0].cpu().numpy(), K.cpu().numpy(),
-                vert_centered[0].cpu().numpy())
+            K = model.iuv2smpl.K[0].cpu().numpy()
+            _, _, img_smpl, smpl_rgba = dr_render.render(image_tensor[0].cpu().numpy(),
+                                                         camera_pred[0].cpu().numpy(), K,
+                                                         verts_pred.cpu().numpy(),
+                                                         smpl_neutral.faces)
 
             img_smpl_rgba = np.concatenate((img_smpl, ones_np), axis=2)
-
             img_vis_list.extend([img_smpl_rgba, smpl_rgba])
 
         img_vis = np.concatenate(img_vis_list, axis=1)
-        img_vis[img_vis<0.0] = 0.0
-        img_vis[img_vis>1.0] = 1.0
-        # omit the forth channel (alpha channel) [transparency]
-        imsave(os.path.join(args.out_dir, img_id + '_result.png'), img_vis[:,:,:3])
+        img_vis[img_vis < 0.0] = 0.0
+        img_vis[img_vis > 1.0] = 1.0
+        imsave(os.path.join(args.out_dir, img_id + '_result.png'), img_vis)
 
 
 if __name__ == '__main__':
--- lib/nn/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .affine import AffineChannel2d
-from .normalization import GroupNorm
-from .upsample import BilinearInterpolation2d
